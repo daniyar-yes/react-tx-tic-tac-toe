8d25424335062efa7bc0ef060a73cd22eee6224a
daniyar-yes <yeskaliyevdan@hotmail.com>
2026-03-23 19:04:24 -0700

    WIP: half way through React tutorial

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,44 @@
-function App() {
+import { useState } from 'react';
 
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    console.log(`hi from event Click on ${value}`);
+    setValue('X')
+  }
+
+  function handleMouseHover() {
+    console.log(`hi from hover event on ${value}`)
+  }
   return (
-    <>
-     <div>Tic Tac Toe game</div>
-    </>
-  )
+    <button 
+      className="square" 
+      onClick={handleClick}
+      onMouseEnter={handleMouseHover}>
+      {value}
+    </button>
+  );
 }
 
-export default App
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
+}
